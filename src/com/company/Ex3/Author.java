fa41d7baff2449f1136f60f926a22360c7695c5f
Mig <yuliya.esaulova2001@mail.ru>
2020-09-19 01:04:10 +0300

    init2

---
+++ src/com/company/Ex3/Author.java
@@ -0,0 +1,38 @@
+package com.company.Ex3;
+
+public class Author {
+    private String name;
+    private String email;
+    private char gender;
+
+    Author(String name, String email, char gender) {
+        this.email = email;
+        this.gender = gender;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorUML{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
